216a491acffcedd546223ab645f2b5cb49de81ab
Borzenkov-Ihor <setty2008@ukr.net>
2023-07-15 00:00:36 +0300

    Тест

---
+++ Cat.java
@@ -0,0 +1,17 @@
+package HomeWork12;
+
+public class Cat extends Animal {
+
+    Integer age;
+
+
+    public Cat(String name) {
+        super(name);
+    }
+
+    public String getName(){
+        return name;
+    }
+
+
+}
